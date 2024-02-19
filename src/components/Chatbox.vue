1e946c18438de32afcd08ddc36f7ef1e6c5cccd0
Hasibur Rahman <hasib.webdev@gmail.com>
2024-02-20 02:41:12 +0600

    Update SVG and Chatbox.vue components

--- src/components/Chatbox.vue
+++ src/components/Chatbox.vue
@@ -3,14 +3,53 @@ import ChatBubbleLeftRight from './icons/ChatBubbleLeftRight.vue';
 import { ref } from 'vue';
 import PaperAirPlane from './icons/PaperAirPlane.vue';
 import { Message } from '../types';
-
+import ThinkingLoader from './ThinkingLoader.vue';
 const API_URL = 'http://localhost:5500/chat';
 
 const isOpen = ref(true);
-const messages = ref<Message[]>([]);
+const isThinking = ref(false);
+const isTyping = ref(false);
+const messages = ref<Message[]>([
+  { role: "assistant", content: "Hello, I'm a chatbot. How can I help you?" }
+]);
 
-async function chat(messages: Message[]): Promise<Message> {
-  console.log("chat");
+let controller = new AbortController();
+let signal = controller.signal;
+
+async function stopChat() {
+  isThinking.value = false;
+  isTyping.value = false;
+
+  controller.abort();
+  controller = new AbortController();
+  signal = controller.signal;
+}
+
+async function streamToString(body: any) {
+  const index = messages.value.length - 1;
+  const reader = body?.pipeThrough(new TextDecoderStream()).getReader();
+  isThinking.value = false;
+  isTyping.value = true;
+  while (reader) {
+    let stream = await reader.read();
+    if (stream.done) break;
+    const chunks = stream.value
+      .replaceAll(/^data: /gm, "")
+      .split("\n")
+      .filter((c: string) => Boolean(c.length) && c !== "[DONE]")
+      .map((c: string) => JSON.parse(c));
+    if (chunks) {
+      for (let chunk of chunks) {
+        const content = chunk.choices[0].delta.content;
+        if (!content) continue;
+        messages.value[index].content += content;
+      }
+    }
+  }
+  isTyping.value = false;
+}
+
+async function chat(messages: Message[]) {
   const body = {
     messages: messages
   };
@@ -20,35 +59,26 @@ async function chat(messages: Message[]): Promise<Message> {
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify(body)
+    body: JSON.stringify(body),
+    signal
   });
 
-  console.log({ response });
+  await streamToString(response.body);
 
-  const reader = response.body?.getReader();
-  if (!reader) {
-    throw new Error("Failed to read response body");
-  }
-  let content = "";
-  while (true) {
-    const { done, value } = await reader.read();
-    if (done) {
-      break;
-    }
-    const rawjson = new TextDecoder().decode(value);
-    const json = JSON.parse(rawjson);
-    console.log({ json });
-    if (json.done === false) {
-      content += json.message.content;
-      console.log({ content });
-    }
 
-  }
-  return { role: "assistant", content: content };
 }
 
-async function send() {
-  messages.value.push(await chat(messages.value));
+async function send(event: Event) {
+  isThinking.value = true;
+
+  const target = event.target as HTMLFormElement;
+  const formData = new FormData(target);
+  const message = formData.get("message");
+  target.reset();
+
+  messages.value.push({ role: "user", content: message as string });
+  messages.value.push({ role: "assistant", content: '' });
+  await chat(messages.value);
 }
 </script>
 
@@ -60,19 +90,53 @@ async function send() {
     </div>
 
     <!-- Box -->
-    <div v-if="isOpen" class="absolute w-[350px] h-[550px] bg-white bottom-[120%] right-[50%] rounded-lg shadow-lg">
-      <div class="h-24 bg-red-50 p-3">
+    <div v-if="isOpen"
+      class="absolute w-[350px] h-[550px] overflow-hidden bg-white bottom-[120%] right-[50%] rounded-lg shadow-lg">
+      <!-- Box Header -->
+      <div class="h-[85px] bg-red-50 p-3">
         <div class="w-10 h-10 bg-teal-50 rounded-full flex justify-center items-center mx-auto">
           🙂
         </div>
         <p class="text-center text-sm text-teal-900 mt-1">Chatbot</p>
       </div>
 
+      <!-- Box messages -->
+      <div class="h-[390px] overflow-y-auto">
+        <div v-for="msg in messages" class="text-sm">
+          <!-- User text -->
+          <div v-if="msg.role === 'user'" class="flex justify-end">
+            <div class="bg-slate-200 text-gray-800 px-3 py-2 rounded-md m-2">
+              {{ msg.content }}
+            </div>
+          </div>
+
+          <!-- Bot text -->
+          <div v-else class="flex justify-start px-3 py-2">
+            <p class="text-2xl" v-if="!msg.content.length && isThinking">🤔</p>
+            <p class="text-2xl" v-else>😎</p>
+            <div class="bg-teal-600 text-white w-full px-3 py-2 rounded-md m-2">
+              <p v-if="msg.content.length">
+                {{ msg.content }}
+              </p>
+              <div v-if="!msg.content.length && isThinking" class="px-4 py-1.5">
+                <ThinkingLoader />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="mt-2 flex justify-center">
+          <button @click="stopChat" v-if="isTyping"
+            class="bg-gray-600 text-xs text-white px-3 py-1.5 rounded-md">Stop</button>
+        </div>
+      </div>
+
+      <!-- Box footer -->
       <div class="absolute bottom-0 w-full border-t px-3 py-3">
         <form @submit.prevent="send" class="flex items-end">
-          <input autofocus autocomplete="off" autocorrect="off" auto type="text"
+          <input autofocus autocomplete="off" autocorrect="off" name="message" auto type="text"
             class="w-full px-3 py-1.5 focus:ring-0 focus:outline-0 placeholder:text-sm flex-1 resize-none " rows="1"
-            placeholder="Compose your message" />
+            placeholder="Compose your message..." />
           <button type="submit" class="bg-teal-600 text-white px-3 py-1.5 rounded-md">
             <PaperAirPlane />
           </button>
---
+++ src/components/ThinkingLoader.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="snippet" data-title="dot-typing">
+        <div class="stage">
+            <div class="dot-typing"></div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.dot-typing {
+    position: relative;
+    left: -9999px;
+    width: 8px;
+    height: 8px;
+    border-radius: 5px;
+    background-color: #fff;
+    color: #fff;
+    box-shadow: 9984px 0 0 0 #fff, 9999px 0 0 0 #fff, 10014px 0 0 0 #fff;
+    animation: dot-typing 1.5s infinite linear;
+}
+
+@keyframes dot-typing {
+    0% {
+        box-shadow: 9984px 0 0 0 #fff, 9999px 0 0 0 #fff, 10014px 0 0 0 #fff;
+    }
+
+    16.667% {
+        box-shadow: 9984px -10px 0 0 #fff, 9999px 0 0 0 #fff, 10014px 0 0 0 #fff;
+    }
+
+    33.333% {
+        box-shadow: 9984px 0 0 0 #fff, 9999px 0 0 0 #fff, 10014px 0 0 0 #fff;
+    }
+
+    50% {
+        box-shadow: 9984px 0 0 0 #fff, 9999px -10px 0 0 #fff, 10014px 0 0 0 #fff;
+    }
+
+    66.667% {
+        box-shadow: 9984px 0 0 0 #fff, 9999px 0 0 0 #fff, 10014px 0 0 0 #fff;
+    }
+
+    83.333% {
+        box-shadow: 9984px 0 0 0 #fff, 9999px 0 0 0 #fff, 10014px -10px 0 0 #fff;
+    }
+
+    100% {
+        box-shadow: 9984px 0 0 0 #fff, 9999px 0 0 0 #fff, 10014px 0 0 0 #fff;
+    }
+}
+</style>
